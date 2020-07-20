11b781ea3911f1024e64ec62ecf81e0731127adf
zg-zhang <493174665@qq.com>
2020-07-20 20:37:29 +0800

    feat：基础配置 font awesome

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const { task, src, dest, watch, series } = require('gulp')
+const connect = require('gulp-connect')
+
+const port = 3000;
+const path = 'src';
+
+function html() {
+  return src('src/**/*.html')
+    .pipe(connect.reload())
+}
+
+function css() {
+  return src('src/**/*.css')
+    .pipe(connect.reload())
+}
+
+function js() {
+  return src('src/**/*.js')
+    .pipe(connect.reload())
+}
+
+// 服务器启动
+function server() {
+  connect.server({
+    name: 'CSS-Secrets',
+    root: path,
+    port: port,
+    livereload: true
+  })
+  watchFile()
+}
+
+function watchFile() {
+  watch(['src/**/*.html', "src/**/*.css", "src/**/*.js"]).on('change', function (path, stats) {
+    console.log(`监听文件变动：${path}`)
+  })
+  watch('src/**/*.html').on('change', html)
+  watch('src/**/*.css').on('change', css)
+  watch('src/**/*.js').on('change', js)
+}
+
+exports.default = server;
